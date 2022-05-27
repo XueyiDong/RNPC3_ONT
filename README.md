5c523e00178993050010022273273474ba412f10
Xueyi Dong <dong.x@wehi.edu.au>
2022-05-27 15:49:44 +1000

    update analysis

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RNPC3_ONT
 RNPC3 ONT RNA-seq analysis
+
+## Files and folders
+[aligned_minimap2](aligned_minimap2): genomic alignment BAM files and scripts for mapping
+[bambu](bambu): scripts and outputs for *bambu*
+[count](count): scripts and outputs from running *featureCounts* for gene-level counting
+[data](data): a soft link to the original data
--- bambu/scripts/run_bambu.R
+++ bambu/scripts/run_bambu.R
@@ -1,6 +1,6 @@
 # bambu pipeline
 
-BiocManager::install(c("bambu", "Rsamtools", "BSgenome"))
+# BiocManager::install(c("bambu", "Rsamtools", "BSgenome"))
 library(Rsamtools)
 library(bambu)
 library(BSgenome)
@@ -13,21 +13,16 @@ CHM13v2_seq = FaFile("/stornext/General/data/user_managed/grpu_mritchie_1/XueyiD
 CHM13v2_gtf = prepareAnnotations("/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/annotation/Human/CHM13v2/GCF_009914755.1_T2T-CHM13v2.0_genomic.gtf")
 
 # BAM files - ONT
-bam_loc <- "/stornext/Projects/promethion/promethion_access/lab_ritchie/A549_RNPC3/long_term/aligned_minimap2"
+bam_loc <- "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/RNPC3/aligned_minimap2"
 bam_file <- list.files(bam_loc, pattern = "bam")
 # bams <- sapply(file.path(bam_loc, bam_file), function(x){
 #   BamFile(x)
 # })
 # merged_ont <- BamFileList(bams)
 
-ont_iso_analysis = bambu(reads = file.path(bam_loc, bam_file), annotations = CHM13v2_gtf, genome = CHM13v2_seq, ncore = 16, verbose = TRUE)
+ont_iso_analysis = bambu(reads = file.path(bam_loc, bam_file), 
+                         annotations = CHM13v2_gtf, genome = CHM13v2_seq, 
+                         ncore = 16, rcOutDir = "../outputs/rc/")
 saveRDS(ont_iso_analysis, "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/RNPC3/bambu/outputs/bambu_out.RDS")
 writeBambuOutput(ont_iso_analysis, path = "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/RNPC3/bambu/outputs")
-
-# testing only
-library(Rsubread)
-fc <- featureCounts(file.path(bam_loc, bam_file[1]),
-              annot.ext = "/stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/annotation/Human/CHM13v2/GCF_009914755.1_T2T-CHM13v2.0_genomic.gtf",
-              isGTFAnnotationFile = TRUE,
-              isLongRead = TRUE,
-              nthreads = 8)
+sessionInfo()
