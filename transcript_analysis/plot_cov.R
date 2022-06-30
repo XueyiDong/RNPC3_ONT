6691b2b3f131a4d62e030a970d67b85adeefd237
Xueyi Dong <dong.x@wehi.edu.au>
2022-06-30 17:35:16 +1000

    use gene region track for tx annotation

--- transcript_analysis/plot_cov.R
+++ transcript_analysis/plot_cov.R
@@ -1,6 +1,7 @@
 library(superintronic)
 suppressPackageStartupMessages(library(plyranges))
 library(ggplot2)
+library(pheatmap)
 
 # prepare annotation----
 gff <- "../bambu/out/extended_annotations.genes.edited.gtf"
@@ -50,69 +51,6 @@ mi_gene <- xlsx::read.xlsx("../HomoSapiens_IntronInfo.xlsx", sheetIndex = 1)
 mi <- read_bed("hglft_genome_b5cb_ee5900.bed")
 mi$gene_id <- tmap$qry_gene_id[match(mi$name, tmap$ref_gene_id)]
 
-add_mi <- function(gene_name, gene_track){
-  out <- gene_track
-  mi_range <- mi %>% filter(name == gene_name)
-  if(length(mi_range) > 0){
-    # cat("Adding", length(mi_range),  "minor introns.\n")
-    for(j in 1:length(mi_range)){
-      out <- out +
-        geom_segment(aes(x = start(mi_range)[j], xend=end(mi_range)[j], y=0.5, yend = 0.5),
-                     size = 10, alpha = 0.5)
-    }
-  }
-  return(out)
-}
-
-# plot for each feature ----
-plot_cov_genes <- function(gene){
-  if (gene %in% parts$gene_id){
-    # cat("Making plot for gene", gene, ".\n")
-    features <- parts %>% filter(gene_id == gene)
-    cvg_over_features <- cvg %>% 
-      select(-Bam) %>% 
-      join_parts(features)
-    if(length(unique(BiocGenerics::strand(unnest_parts(features)))) > 1) {
-      cat("Gene", gene, "strand not unique.\n")
-    } else{
-      gene_track <- view_segments(unnest_parts(features),
-                                  colour = feature_type)
-      # mark retained introm for mi gene
-      gene_name <- tmap$ref_gene_id[match(gene, tmap$qry_gene_id)]
-      gene_track <- add_mi(gene_name, gene_track)
-      p <- cvg_over_features %>% 
-        mutate(strand = feature_strand) %>% 
-        view_coverage(score = score, 
-                      colour = feature_type, 
-                      facets = vars(siRNA)) + 
-        scale_color_brewer(palette = "Dark2") +
-        guides(colour = FALSE) +
-        labs(title = gene)
-      p / gene_track + patchwork::plot_layout(heights = c(3, 1))
-    }
-  } else {
-    cat("Gene", gene, "not found.\n")
-  }
-}
-
-# make plot ---
-genes <- list()
-for(i in c("DTEgenesMI", "DTEgenesNotMI", "DTUgenesMI", "DTUgenesNotMI")){
-  cat("Working on", i, ".\n")
-  genes[[i]] <- readRDS(paste0(i, ".RDS"))
-  pdf(paste0("plots/cov_", i, ".pdf"))
-  for(j in genes[[i]]){
-    print(plot_cov_genes(j))
-  }
-  dev.off()
-}
-
-pdf("plots/cov_allMI.pdf")
-for(i in unique(na.omit(tmap$qry_gene_id[match(mi_gene$Gene.Name, tmap$ref_gene_id)]))){
-  print(plot_cov_genes(i))
-}
-dev.off()
-
 # Visualize coverage using Gviz ----
 library(Gviz)
 
@@ -149,12 +87,10 @@ plot_cov_genes2 <- function(gene, cov_data = c("long", "short"), anno_col = "tra
         # axis
         axisTrack <- GenomeAxisTrack()
         # transcripts annotation
-        anno <- gr %>% filter(gene_id == gene, type=="exon")
-        aTrack <- AnnotationTrack(anno, 
-                                  group = anno %>% as.data.frame %>% dplyr::pull(anno_col),
-                                  options(ucscChromosomeNames=FALSE),
-                                  name = "Isoforms")
-        trackList = aTrack
+        trackList = GeneRegionTrack(gr %>% 
+                                      filter(gene_id==gene) %>% 
+                                      GenomicFeatures::makeTxDbFromGRanges(),
+                                    transcriptAnnotation = "transcript")
         # add coverage histogram track
         if("long" %in% cov_data){
           dTrack_long <- make_cov_track(cvg, gene, "long")
@@ -178,6 +114,7 @@ plot_cov_genes2 <- function(gene, cov_data = c("long", "short"), anno_col = "tra
     }
 }
 
+
 # add DTE/DTU info and isoform category to annotation ----
 tt <- read.csv("results/DE_topTags.csv")
 ts <- read.csv("results/DTU_transcript_topSplice.csv")
@@ -190,7 +127,7 @@ gr$transcript_id_status[gr$isDTU] <- paste(gr$transcript_id_status[gr$isDTU], "(
 gr$class_code <- tmap$class_code[match(gr$transcript_id, tmap$qry_id)]
 gr$transcript_id_status <- paste(gr$transcript_id_status, gr$class_code)
 
-# make plot using gviz ---
+# make plot for DTE/DTU using gviz ----
 # genes <- list()
 for(i in c("DTEgenesMI", "DTEgenesNotMI", "DTUgenesMI", "DTUgenesNotMI")){
   cat("Working on", i, ".\n")
@@ -202,6 +139,8 @@ for(i in c("DTEgenesMI", "DTEgenesNotMI", "DTUgenesMI", "DTUgenesNotMI")){
   dev.off()
 }
 
+# make plot for Stephen's gene lists
+
 # plot for non DTE/DTU minor intron genes
 mi_nosig_genes <- unique(na.omit(tmap$qry_gene_id[match(mi_gene$Gene.Name, tmap$ref_gene_id)]))
 mi_nosig_genes <- mi_nosig_genes[!(mi_nosig_genes %in% union(genes$DTEgenesMI, genes$DTUgenesMI))]
@@ -210,11 +149,12 @@ mi_nosig_genes <- mi_nosig_genes[sapply(mi_nosig_genes, function(x){
 }, simplify = TRUE)]
 pdf("plots/cov_notSigMI_mnj.pdf")
 for(i in mi_nosig_genes){
-  plot_cov_genes2(i)
+  plot_cov_genes2(i, anno_col = "transcript_id_status")
 }
 dev.off()
 
 # test----
+plot_cov_genes2("TMEM80_1", anno_col = "transcript_id_status")
 plot_cov_genes2("TMEM80_1")
 plot_cov_genes2("UBL5_1")
 plot_cov_genes2("SPCS2_1")
