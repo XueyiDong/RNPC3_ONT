27a87595215ee88e2b5a0fc738e59070782de970
Xueyi Dong <dong.x@wehi.edu.au>
2022-07-05 16:09:38 +1000

    add script for salmon alignment mode for long read

--- salmon/scripts/run_salmon_quant_long_align.sh
+++ salmon/scripts/run_salmon_quant_long_align.sh
@@ -6,6 +6,8 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=salmon_quant_long_bambu.log
 
+# this is the final version used
+
 cd /stornext/General/data/user_managed/grpu_mritchie_1/XueyiDong/RNPC3/salmon
 
 export PATH=/home/users/allstaff/dong.x/Programs/salmon/salmon-1.9.0_linux_x86_64/bin:$PATH
@@ -15,5 +17,6 @@ FQ=/stornext/Projects/promethion/promethion_access/lab_ritchie/A549_RNPC3/long_t
 for SAMPLE in barcode{01..08}
 do
 	mkdir -p ./long_bambu/$SAMPLE
-	salmon quant -i salmon_index -l A -r $FQ/$SAMPLE\_pass.fq.gz $FQ/$SAMPLE\_fail.fq.gz --validateMappings -o ./long_bambu/$SAMPLE -p 16 --numBootstraps 100
+	salmon quant -t bambu_transcripts.fa -l A -a ../tx_aligned_bambu_minimap2/$SAMPLE\_pass.bam ../tx_aligned_bambu_minimap2/$SAMPLE\_fail.bam -o ./long_bambu/$SAMPLE -p 16 --numBootstraps 100
+	# salmon quant -i salmon_index -l A -r $FQ/$SAMPLE\_pass.fq.gz $FQ/$SAMPLE\_fail.fq.gz --validateMappings -o ./long_bambu/$SAMPLE -p 16 --numBootstraps 100
 done
